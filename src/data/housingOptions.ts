dbb4b89762311600d08c82a57a3047b6de0ddacc
aumerov <89824787+aumerov@users.noreply.github.com>
2024-06-05 11:45:18 -0700

    Added Data

--- src/data/housingOptions.ts
+++ src/data/housingOptions.ts
@@ -1,10 +1,10 @@
 export const housingOptions = [
     {
-        id: 2,
+        id: 1,
         image: "https://images.cdn.appfolio.com/rpmca036/images/f5375d2e-5131-492b-b824-6cb54b691275/medium.jpg",
         landlordName: "RPM Central Coast",
         address: "580 Pacific St, San Luis Obispo, CA 93401",
-        overallRating: 5,
+        overallRating: 3,
         reviews: [
             {
                 rating: 3,
@@ -30,26 +30,64 @@ export const housingOptions = [
         ]
     },
     {
-        id: 3,
+        id: 2,
         image: "https://img.zumpercdn.com/715422311/1280x960?dpr=1&fit=crop&h=542&q=76&w=991",
         landlordName: "California-West, Inc.",
         address: "1850 Johnson Ave, San Luis Obispo, CA 93401",
-        overallRating: 4,
-        reviews: []
+        overallRating: 3.5,
+        reviews: [
+            {
+                rating: 4,
+                landlordRating: 4,
+                housingRating: 4,
+                likes: "2 story apartment, quiet neighbors, double parking, had a storage",
+                dislikes: "It was on the street, pretty loud. "
+            },
+            {
+                rating: 3,
+                landlordRating: 2,
+                housingRating: 4,
+                likes: "Love walking around the nighborhood. Quite close to nature.",
+                dislikes: "You won't get your security deposit back. Landlords didn't do anything about our rat problem. Across from a hospital."
+            }
+        ]
     },
     {
-        id: 1,
+        id: 3,
         image: "https://www.dci-engineers.com/uploads/projects/_1200x600_crop_center-center_none/CalPolySLOHousing_PhotoByBruceDamonte_09.jpg",
         landlordName: "Cal Poly University",
         address: "1 Grand Ave, San Luis Obispo, CA 93401",
+        overallRating: 4,
+        reviews: [
+            {
+                rating: 4,
+                landlordRating: 4,
+                housingRating: 4,
+                likes: "GReally close to my classes.",
+                dislikes: "Campus food is awful."
+            }
+        ]
+    },
+    {
+        id: 4,
+        image: "https://images.cdn.appfolio.com/regpm/images/b973671c-9070-4cef-a891-dfceda5238af/medium.jpeg",
+        landlordName: "REG Property Management",
+        address: "1243 Bond Street - Unit #B, San Luis Obispo, CA 93405",
         overallRating: 3,
         reviews: [
             {
-                rating: 5,
+                rating: 3,
+                landlordRating: 3,
+                housingRating: 3,
+                likes: "Its cute, close to my job, and relatively new. ",
+                dislikes: "My room had a leak in the window when it was raining and mold formed."
+            },
+            {
+                rating: 4.5,
                 landlordRating: 4,
-                housingRating: 4,
-                likes: "Great location and view.",
-                dislikes: "Can be noisy at night."
+                housingRating: 5,
+                likes: "I like the location, my house is pretty big, and it has laundry and a dishwasher included.",
+                dislikes: "Its outdated and the tile in the kitchen is so ugly. We also can't have pets. "
             }
         ]
     },
